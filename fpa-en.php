fa983c93788fac8338f331e47f22a31cb12e6c7a
RussW <winterrg@gmail.com>
2018-05-27 20:15:09 +1000

    added meta robots : noindex, nofollow
    try to avoid FPA being indexed (by ethical robots) on sites where the owner has been lazy and left it onstalled

--- fpa-en.php
+++ fpa-en.php
@@ -381,6 +381,8 @@ $disabled = '';
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
+        <meta name="robots" content="noindex, nofollow />
+
         <title><?php echo _RES .' : '. _FPA_VER_SHORT .''. _RES_VERSION .'.'. _RES_VERSION_MAINT .' ['. _RES_RELEASE_TYPE .'] '. _RES_RELEASE_BUILD .' '. _RES_LANG; ?></title>
 
         <?php
